87303f82ed22778e2f57d23793f3949789940cb0
Andreas Andreou <aandreou@thoughtworks.com>
2019-02-06 21:09:08 +0000

    [Andreas] Create project

---
+++ src/main/java/com/andreouconsulting/skyparentalcontrol/SkyParentalControlApplication.java
@@ -0,0 +1,14 @@
+package com.andreouconsulting.skyparentalcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyParentalControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyParentalControlApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/andreouconsulting/skyparentalcontrol/SkyParentalControlApplicationTests.java
@@ -0,0 +1,17 @@
+package com.andreouconsulting.skyparentalcontrol;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SkyParentalControlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
